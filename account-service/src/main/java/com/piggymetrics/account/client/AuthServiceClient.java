75aa5723f31fea0e9050414d23ab26ed845a126e
Admin <Admin@PF2NDXQG-NYK.inc.bytedance.com>
2021-12-23 14:41:50 +0800

    use dns

--- account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java
+++ account-service/src/main/java/com/piggymetrics/account/client/AuthServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "auth-service")
+@FeignClient(url="http://auth-service:5000",name = "auth.service")
 public interface AuthServiceClient {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/uaa/users", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
